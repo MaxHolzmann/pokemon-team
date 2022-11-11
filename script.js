8337fbc54c26feb165b356e970963892f929a987
MaxHolzmann <max@holzmann.io>
2022-11-11 16:38:28 -0600

    comments out current card click logic

--- script.js
+++ script.js
@@ -47,6 +47,8 @@ const editPokemon = (data, i) => {
     if(data.sprites.other.home.front_default === null) {
         cardContainer.children[i].children[1].setAttribute("src", data.sprites.other["official-artwork"].front_default)
 }
+    localStorage.setItem(i, JSON.stringify(data));
+
 }
 
 const firstLetterCapital = (str) => {
@@ -70,7 +72,20 @@ const checkGens = () => {
 
 cardContainer.addEventListener("click", (e) => {
     if(!e.target.classList.contains('card-container')) {
-        //flip here
+        if(e.target.childElementCount === 0) {
+            if(e.target.parentElement.classList.contains('card-flip')) {
+                e.target.parentElement.classList.remove('card-flip');
+                
+            } else {
+            e.target.parentElement.classList.add('card-flip');
+            e.target.parentElement.children[0].textContent = 'hey';
+            e.target.parentElement.children[1].setAttribute('src', "")
+        }
+        } else {
+            e.target.classList.add('card-flip');
+            e.target.parentElement.children[0].textContent = 'hey';
+            e.target.parentElement.children[1].setAttribute('src', "")
+        }
     } else {
         console.log('container')
     }
